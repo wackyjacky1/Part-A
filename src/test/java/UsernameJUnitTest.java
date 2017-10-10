c0d0d47009e5f5f9e02a44e8d6596d67ef744c67
Jack O'Loughlin <32545161+wackyjacky1@users.noreply.github.com>
2017-10-10 18:24:47 +0100

    Update UsernameJUnitTest.java

--- src/test/java/UsernameJUnitTest.java
+++ src/test/java/UsernameJUnitTest.java
@@ -22,7 +22,7 @@ public class UsernameJUnitTest {
 	public void test() {
             
                 DateTime s1_start_date = new DateTime(2014, 4,9,12,0,0,0);
-                DateTime s1_finish_date = new DateTime(2018, 28,5,12,0,0,0);
+                DateTime s1_finish_date = new DateTime(2018, 9,5,12,0,0,0);
                 
 		Student s1 = new Student("Sean","Kelly","15 Nov 1996", 1443535, 20, "Computer Science and IT",s1_start_date , s1_finish_date, "Programming","CT213" );
 		String output = s1.getusername("Sean", "Kelly", 20);
